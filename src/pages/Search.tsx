993fecdcbdc612f2a356f26293b3ae1d646a87ed
Pantai01 <a01276114@tec.mx>
2025-05-27 17:39:38 -0600

    Update: Removed hardcoded interface.

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,12 +1,8 @@
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { fetchGamesSearch } from "../services/gamesService";
-
-interface Game {
-  id: number;
-  name: string;
-  background_image: string;
-}
+import Card from "../components/Card";
+import type { Game } from "../interfaces/Game";
 
 export default function Search() {
   const [searchParams] = useSearchParams();
@@ -30,10 +26,14 @@ export default function Search() {
       <h1 className="text-2xl mb-4">Results for: <span className="italic">{query}</span></h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {games.map((game) => (
-          <div key={game.id} className="bg-gray-800 p-4 rounded shadow hover:bg-gray-700 transition">
-            <img src={game.background_image} alt={game.name} className="w-full h-48 object-cover rounded mb-2" />
-            <h2 className="text-xl">{game.name}</h2>
-          </div>
+          <Card
+            id={game.id}
+            name={game.name}
+            background_image={game.background_image}
+            rating={game.rating}
+            playtime={game.playtime}
+            description={game.description}
+          />
         ))}
       </div>
     </div>
